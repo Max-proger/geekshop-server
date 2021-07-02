dfcf3a216efe4da3103ef61e486bcee37e6cc48c
Max <gatts@mail.ru>
2021-07-03 01:31:26 +0300

    added dynamic content on the index and products pages from a json file

--- products/views.py
+++ products/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+import json
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {'title': 'GeekShop'}
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {'title': 'GeekShop - Каталог'}
+    with open('products/fixtures/goods.json', encoding='utf-8') as json_file:
+        context['products'] = json.load(json_file)
+    return render(request, 'products/products.html', context)
