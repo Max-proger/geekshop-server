454f34054a5b521a3bfe5abfd3221265f78d2205
Max <gatts@mail.ru>
2021-07-07 01:53:24 +0300

    registered models for admin panel

--- products/models.py
+++ products/models.py
@@ -5,6 +5,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=250)
@@ -13,3 +16,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
