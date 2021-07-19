318555f51d23ef4598f93936f1e79edec8442c93
Max <gatts@mail.ru>
2021-07-19 18:30:47 +0300

    added "update" for users

--- admins/views.py
+++ admins/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from users.models import User
-from admins.forms import UserAdminRegistrationForm
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
 def index(request):
@@ -29,8 +29,22 @@ def admin_users_create(request):
     return render(request, 'admins/admin-users-create.html', context)
 
 
-def admin_users_update(request, pk=None):
-    return render(request, 'admins/admin-users-update-delete.html')
+def admin_users_update(request, pk):
+    selected_user = User.objects.get(id=pk)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {
+        'title': 'Админ-панель - Редактирование пользовтаеля',
+        'form': form,
+        'selected_user': selected_user,
+    }
+    return render(request, 'admins/admin-users-update-delete.html', context)
+
 
 def admin_users_remove(request, pk=None):
     pass
