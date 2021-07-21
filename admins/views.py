362f6d64fcb31b52318642216b7be92ba4454beb
Max <gatts@mail.ru>
2021-07-22 01:04:44 +0300

    overwrite get-context_date() for UserCreateView, UserUpdateView and dispatch() for UserListView, UserCreateView, UserUpdateView, UserDeleteView

--- admins/views.py
+++ admins/views.py
@@ -5,6 +5,7 @@ from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.utils.decorators import method_decorator
 
 @user_passes_test(lambda u: u.is_staff)
 def index(request):
@@ -20,17 +21,39 @@ class UserListView(ListView):
         context['title'] = 'Админ-панель - Пользователи'
         return context
 
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
+
 class UserCreateView(CreateView):
     model = User
     form_class = UserAdminRegistrationForm
     template_name = 'admins/admin-users-create.html'
     success_url = reverse_lazy('admins:admin_users')
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UserCreateView, self).get_context_data(object_list=None, **kwargs)
+        context['title'] = 'Админ-панель - Создание пользователя'
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
+
 class UserUpdateView(UpdateView):
     model = User
     form_class = UserAdminProfileForm
     template_name = 'admins/admin-users-update-delete.html'
     success_url = reverse_lazy('admins:admin_users')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UserUpdateView, self).get_context_data(object_list=None, **kwargs)
+        context['title'] = 'Админ-панель - Редакрование пользователя'
+        return context
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserUpdateView, self).dispatch(request, *args, **kwargs)
 
 
 class UserDeleteView(DeleteView):
@@ -43,3 +66,7 @@ class UserDeleteView(DeleteView):
         self.object.is_active = False
         self.object.save()
         return HttpResponseRedirect(self.get_success_url())
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserDeleteView, self).dispatch(request, *args, **kwargs)
