4e9200388ef3ede588e9d3a537703306c10f40a9
Max <gatts@mail.ru>
2021-07-22 00:06:13 +0300

    added CBV for UpdateView

--- admins/views.py
+++ admins/views.py
@@ -4,7 +4,7 @@ from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 @user_passes_test(lambda u: u.is_staff)
 def index(request):
@@ -21,24 +21,12 @@ class UserCreateView(CreateView):
     template_name = 'admins/admin-users-create.html'
     success_url = reverse_lazy('admins:admin_users')
 
+class UserUpdateView(UpdateView):
+    model = User
+    form_class = UserAdminProfileForm
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admins:admin_users')
 
-
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_update(request, pk):
-    selected_user = User.objects.get(id=pk)
-    if request.method == 'POST':
-        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminProfileForm(instance=selected_user)
-    context = {
-        'title': 'Админ-панель - Редактирование пользовтаеля',
-        'form': form,
-        'selected_user': selected_user,
-    }
-    return render(request, 'admins/admin-users-update-delete.html', context)
 
 @user_passes_test(lambda u: u.is_staff)
 def admin_users_remove(request, pk):
