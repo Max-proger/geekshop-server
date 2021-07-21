92b2aa6350c67ace12223098e8cb18427b7438e3
Max <gatts@mail.ru>
2021-07-21 23:52:40 +0300

    added CBV for ListView and CreateView

--- admins/views.py
+++ admins/views.py
@@ -1,32 +1,27 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 from django.contrib.auth.decorators import user_passes_test
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 
+@user_passes_test(lambda u: u.is_staff)
 def index(request):
     context = {'title': 'Админ-панель'}
     return render(request, 'admins/index.html', context)
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users(request):
-    context = {'title': 'Админ-панель - Пользовтаели', 'users': User.objects.all()}
-    return render(request, 'admins/admin-users-read.html', context)
+class UserListView(ListView):
+    model = User
+    template_name = 'admins/admin-users-read.html'
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminRegistrationForm()
-    context = {
-        'title': 'Админ-панель - Создание пользователя',
-        'form': form,
-    }
-    return render(request, 'admins/admin-users-create.html', context)
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserAdminRegistrationForm
+    template_name = 'admins/admin-users-create.html'
+    success_url = reverse_lazy('admins:admin_users')
+
+
 
 @user_passes_test(lambda u: u.is_staff)
 def admin_users_update(request, pk):
